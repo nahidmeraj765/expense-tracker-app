a84d18b21e9834fa5066ffcb7c9a52e85130ce8d
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-06-02 13:09:08 +0600

    adding unique value and enum functionalities

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/expenses.dart';
+
+main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
